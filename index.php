edfe8b9c5e60ce2245a193d1d8b8522f7b684139
Novrii <38958308+Novrii@users.noreply.github.com>
2018-09-21 22:56:07 +0700

    On Progress

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ use Stichoza\GoogleTranslate\TranslateClient;
 ?>
 <html>
 <head>
-  <title>Home</title>
+  <title>Arabic Translation</title>
   <link rel="icon" href="images/arabic-icon.png">
   <link rel="stylesheet" href="bulma/css/bulma.min.css">
   <script defer src="https://use.fontawesome.com/releases/v5.1.0/js/all.js"></script>
@@ -27,30 +27,34 @@ use Stichoza\GoogleTranslate\TranslateClient;
 </section>
 
   <div class="container">
+
     <form method="post" action="">
       <div class="columns">
         <div class="column">
-          <div class="media" style="margin-bottom: 14px;">
-
-            <h3>Indonesia</h3>
+          <div class="media" style="margin-bottom: 10px;">
+            <div class="buttons has-addons">
+              <button class="button is-info is-selected">Indonesia</button>
+              <button class="button">Arab</button>
+            </div>
           </div>
           <textarea class="textarea is-info" id="id" name="id">
 
           </textarea>
 
           <p class="content is-small" >Ketik teks atau <a href="#" >Terjemahkan file <i class="fa fa-upload" aria-hidden="true" style="padding-left: 3px;"></i></a></p>
-
-
-        </div>
+        </div>      
         <div class="column">
 
-          <div class="media" style="margin-bottom: 10px;">
-            <div class="media-left">
-              <h5>Arab</h5>
-            </div>
-            <div class="media-right">
-              <button class="button is-info is-small" id="translate" name="translate" onclick="myFunction()">Terjemahkan</button>
-            </div>
+          <div class="media" style="margin-bottom: -15px;">
+
+              <div class="buttons has-addons is-left">
+                <button class="button">Indonesia</button>
+                <button class="button is-info is-selected">Arab</button>
+              </div>
+              <div class="buttons" style="margin-left: 30px;">
+                <button class="button is-info" id="translate" name="translate" onclick="myFunction()">Terjemahkan</button>
+              </div>
+
           </div>
 
           <textarea class="textarea is-success" id="ar" name="ar">
@@ -61,7 +65,8 @@ use Stichoza\GoogleTranslate\TranslateClient;
                     $sourceText = $_POST['id'];
                     $tr = new TranslateClient("id","ar");
                     $tr->setUrlBase('https://translate.googleapis.com/translate_a/single');
-                    echo $tr->translate($sourceText);
+                    $hasil = $tr->translate($sourceText);
+                    echo "$hasil";
                 }
                 ?>
           </textarea>
@@ -72,13 +77,14 @@ use Stichoza\GoogleTranslate\TranslateClient;
       </div>
 
     </form>
+
   </div>
   <script type="text/javascript">
     function myFunction(){
       var indo = document.getElementById("id").value;
       var arab = document.getElementById("ar").value;
 
-      document.getElementById("demo").innerHTML = arab;
+      document.getElementById("demo").innerHTML = indo;
 
 
 
